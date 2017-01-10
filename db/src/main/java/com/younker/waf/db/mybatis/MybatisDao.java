aa16c52a74e5aecf9aed8e00887c50f7624cfaa9
niyuzhe <niyuzhe@wayeasoft.com>
2017-01-10 17:26:22 +0800

    add some debug info and delete the request sqlsession files

--- db/src/main/java/com/younker/waf/db/mybatis/MybatisDao.java
+++ db/src/main/java/com/younker/waf/db/mybatis/MybatisDao.java
@@ -2,8 +2,10 @@ package com.younker.waf.db.mybatis;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,6 +26,7 @@ import com.google.common.reflect.ClassPath.ClassInfo;
 import com.nbm.exception.NbmBaseRuntimeException;
 import com.nbm.waf.core.modeldriven.Mapper;
 import com.younker.waf.db.DataSourceProvider;
+import com.younker.waf.utils.StringUtil;
 
 /**
  * 通用Dao，底层调用Mybatis相关工具。 依赖MybatisConfig类的初始化工作。
@@ -34,287 +37,310 @@ import com.younker.waf.db.DataSourceProvider;
  */
 public enum MybatisDao
 {
-	INSTANCE;
+        INSTANCE;
 
-	private final static Logger log = LoggerFactory
-	                .getLogger(MybatisDao.class);
+        private final static Logger log = LoggerFactory
+                        .getLogger(MybatisDao.class);
 
-	static AtomicInteger sessionCount = new AtomicInteger(0);
+        static AtomicInteger sessionCount = new AtomicInteger(0);
 
-	private MybatisDao()
-	{
-	}
+        private MybatisDao()
+        {
+        }
 
-	// public static final MybatisDao INSTANCE = new MybatisDao();
+        // public static final MybatisDao INSTANCE = new MybatisDao();
 
-	private Map<Thread, SqlSession> sqlSessionByThread = new HashMap<>();
+        private Map<Thread, SqlSession> sqlSessionByThread = new HashMap<>();
 
-	private Map<HttpServletRequest, SqlSession> sqlSessionByRequest = new HashMap<>();
+//        private Map<HttpServletRequest, SqlSession> sqlSessionByRequest = new HashMap<>();
 
-	private SqlSessionFactory sessionFactory;
+        private SqlSessionFactory sessionFactory;
 
-	/**
-	 * 逐步向默认配置文件路径的方向过渡
-	 * 
-	 * @param configFile
-	 * @throws IOException
-	 */
-	@Deprecated
-	public void init(URL configFile) throws IOException
-	{
-		try
-		{
-			sessionFactory = new SqlSessionFactoryBuilder()
-			                .build(configFile.openStream());
-			log.info("Mybatis初始化成功");
-		} catch (IOException e)
-		{
-			// 发生配置文件不能获取的异常后，此处无法解决问题，直接转换为运行时异常抛出
-			log.error("初始化Mybatis发生异常，将造成项目整体不能运行[configFile="
-			                + configFile + "].", e);
-			throw new RuntimeException(e);
-		}
-
-	}
-
-	/**
-	 * 在classpath中找configuration.xml
-	 * 
-	 * @throws IOException
-	 */
-	public void init()
-	{
-		try
-		{
-			sessionFactory = new SqlSessionFactoryBuilder()
-			                .build(Resources.getResourceAsReader("mybatis-config.xml"));
-			log.info("Mybatis初始化成功");
-		} catch (IOException e)
-		{
-			// 发生配置文件不能获取的异常后，此处无法解决问题，直接转换为运行时异常抛出
-			log.error("初始化Mybatis发生异常，将造成项目整体不能运行[configFile=mybatis-config.xml].",
-			                e);
-			throw new RuntimeException(e);
-		}
-
-	}
-
-	
-	/**
-	 * 自动搜索所有的配置文件并初始化
-	 * @param packageName
-	 */
-	public void initAuto(String packageName)
-	{
-		TransactionFactory transactionFactory = new JdbcTransactionFactory();
-		Environment environment = new Environment("development",
-		                transactionFactory, DataSourceProvider
-		                                .instance().getDataSource());
-		Configuration configuration = new Configuration(environment);
-
-		try
+        /**
+         * 逐步向默认配置文件路径的方向过渡
+         * 
+         * @param configFile
+         * @throws IOException
+         */
+        @Deprecated
+        public void init(URL configFile) throws IOException
+        {
+                try
                 {
-	                for (ClassInfo classInfo : ClassPath.from(getClass().getClassLoader()).getTopLevelClassesRecursive(packageName))
-	                {
-	                	Class<?> modelClass = classInfo.load();
-	                	// log.info("start class[{}]", className.getName());
-	                	if (Mapper.class.isAssignableFrom(modelClass)
-	                	                && !modelClass.isAnonymousClass() 
-	                	                && !modelClass.equals(Mapper.class)
-	                	                //&& !modelClass.isInterface()
-	                	                //&& !Modifier.isAbstract(modelClass.getModifiers())
-	                	                )
-	                	{
-	                		log.debug("find Mapper and add: {}", modelClass);
-	                		configuration.addMapper(modelClass);
-	                	}
-	                }
+                        sessionFactory = new SqlSessionFactoryBuilder()
+                                        .build(configFile.openStream());
+                        log.info("Mybatis初始化成功");
                 } catch (IOException e)
                 {
-	                throw new NbmBaseRuntimeException("", e);
+                        // 发生配置文件不能获取的异常后，此处无法解决问题，直接转换为运行时异常抛出
+                        log.error("初始化Mybatis发生异常，将造成项目整体不能运行[configFile="
+                                        + configFile + "].", e);
+                        throw new RuntimeException(e);
                 }
-		
-		 MybatisDao.INSTANCE.initFactory(new SqlSessionFactoryBuilder()
-		 	.build(configuration));
 
-	}
-	
+        }
 
-	/**
-	 * 获取SessionFactory。可考虑在以后的重构中，将此函数改为private，并在Dao类中提供session的相关方法
-	 */
-	private synchronized SqlSession getSession()
-	{
+        /**
+         * 在classpath中找configuration.xml
+         * 
+         * @throws IOException
+         */
+        public void init()
+        {
+                try
+                {
+                        sessionFactory = new SqlSessionFactoryBuilder()
+                                        .build(Resources.getResourceAsReader(
+                                                        "mybatis-config.xml"));
+                        log.info("Mybatis初始化成功");
+                } catch (IOException e)
+                {
+                        // 发生配置文件不能获取的异常后，此处无法解决问题，直接转换为运行时异常抛出
+                        log.error("初始化Mybatis发生异常，将造成项目整体不能运行[configFile=mybatis-config.xml].",
+                                        e);
+                        throw new RuntimeException(e);
+                }
 
-		if (sessionFactory == null)
-		{
-			throw new IllegalArgumentException(
-			                "SqlSessionFactory为空，Mybatis尚未初始化");
-		}
+        }
 
-		SqlSession sqlsession = sessionFactory.openSession();
-		sessionCount.getAndIncrement();
-		log.debug("新打开一个sqlsession，当前数量为：" + sessionCount.get());
-		return sqlsession;
-	}
+        /**
+         * 自动搜索所有的配置文件并初始化
+         * 
+         * @param packageName
+         */
+        public void initAuto(String packageName)
+        {
+                TransactionFactory transactionFactory = new JdbcTransactionFactory();
+                Environment environment = new Environment("development",
+                                transactionFactory,
+                                DataSourceProvider.instance().getDataSource());
+                Configuration configuration = new Configuration(environment);
 
-	synchronized SqlSession getSession(Thread thread)
-	{
+                try
+                {
+                        for (ClassInfo classInfo : ClassPath
+                                        .from(getClass().getClassLoader())
+                                        .getTopLevelClassesRecursive(
+                                                        packageName))
+                        {
+                                Class<?> modelClass = classInfo.load();
+                                // log.info("start class[{}]",
+                                // className.getName());
+                                if (Mapper.class.isAssignableFrom(modelClass)
+                                                && !modelClass.isAnonymousClass()
+                                                && !modelClass.equals(
+                                                                Mapper.class)
+                                // && !modelClass.isInterface()
+                                // &&
+                                // !Modifier.isAbstract(modelClass.getModifiers())
+                                )
+                                {
+                                        log.debug("find Mapper and add: {}",
+                                                        modelClass);
+                                        configuration.addMapper(modelClass);
+                                }
+                        }
+                } catch (IOException e)
+                {
+                        throw new NbmBaseRuntimeException("", e);
+                }
 
-		SqlSession session = getSession();
-		sqlSessionByThread.put(thread, session);
+                MybatisDao.INSTANCE.initFactory(new SqlSessionFactoryBuilder()
+                                .build(configuration));
 
-		return session;
+        }
 
-	}
+        /**
+         * 获取SessionFactory。可考虑在以后的重构中，将此函数改为private，并在Dao类中提供session的相关方法
+         */
+        private synchronized SqlSession getSession()
+        {
 
-	synchronized SqlSession getSession(HttpServletRequest request)
-	{
+                if (sessionFactory == null)
+                {
+                        throw new IllegalArgumentException(
+                                        "SqlSessionFactory为空，Mybatis尚未初始化");
+                }
 
-		SqlSession session = getSession();
-		sqlSessionByRequest.put(request, session);
+                SqlSession sqlsession = sessionFactory.openSession();
+                sessionCount.getAndIncrement();
+                log.debug("新打开一个sqlsession，当前数量为：" + sessionCount.get());
+                debugSessionInfo("");
+                return sqlsession;
+        }
 
-		return session;
+        synchronized SqlSession getSession(Thread thread)
+        {
 
-	}
+                SqlSession session = getSession();
+                sqlSessionByThread.put(thread, session);
 
-	private void debugSessionInfo(String info)
-	{
-		// try
-		// {
-		// StringBuilder sb = new StringBuilder("sqlsession信息\n" + info
-		// + "\n");
-		// sb.append(StringUtil.getString("thread sqlsession池中共有session{}个",
-		// sqlSessionByThread.size())).append("\n");
-		// for (Entry<Thread, SqlSession> entry :
-		// Collections.unmodifiableMap(
-		// sqlSessionByThread).entrySet())
-		// {
-		// sb.append(StringUtil.getString("线程id[{}]， 线程状态[{}]",
-		// entry.getKey()
-		// .toString(), entry.getKey().getState())).append(
-		// "\n");
-		// if (entry.getKey().getState() == Thread.State.TERMINATED)
-		// {
-		// sb.append("sqlsession泄露\n");
-		// }
-		// }
-		//
-		// sb.append(StringUtil.getString("request sqlsession池中共有session{}个",
-		// sqlSessionByRequest.size())).append("\n");
-		// for (Entry<HttpServletRequest, SqlSession> entry :
-		// Collections
-		// .unmodifiableMap(sqlSessionByRequest).entrySet())
-		// {
-		// User user = RuntimeUserUtils.INSTANCE.getUserByRequest(entry
-		// .getKey());
-		// String loginname = user == null ? "" : user.getLoginname();
-		// sb.append(StringUtil.getString("request[{}]，user[{}]", entry
-		// .getKey().getRequestURI(), loginname)).append("\n");
-		// }
-		//
-		// log.debug(sb.toString());
-		// } catch (Exception e)
-		// {
-		// log.error("打印异常调试信息时出现异常", e);
-		// }
-	}
+                return session;
 
-	// public SqlSession getBatchSession()
-	// {
-	// if (sessionFactory == null)
-	// {
-	// throw new
-	// IllegalArgumentException("SqlSessionFactory为空，Mybatis尚未初始化");
-	// }
-	//
-	// return sessionFactory.openSession(ExecutorType.BATCH);
-	// }
+        }
 
-	private void closeSession(SqlSession session)
-	{
-		try
-		{
-			session.commit();
-		} catch (Exception e)
-		{
-			log.error("提交到数据库发生错误", e);
-		} finally
-		{
-			session.close();
-			sessionCount.getAndDecrement();
-			log.debug("关闭一个sqlsession，当前数量为：" + sessionCount.get());
-		}
-	}
+//        synchronized SqlSession getSession(HttpServletRequest request)
+//        {
+//
+//                SqlSession session = getSession();
+//                sqlSessionByRequest.put(request, session);
+//
+//                return session;
+//
+//        }
 
-	void closeSession(SqlSession session, Thread thread)
-	{
-		closeSession(session);
-		sqlSessionByThread.remove(thread);
-	}
+        private void debugSessionInfo(String info)
+        {
+                if( log.isDebugEnabled())
+                {
+                try
+                {
+                        StringBuilder sb = new StringBuilder(
+                                        "sqlsession信息\n" + info + "\n");
+                        sb.append(StringUtil.getString(
+                                        "thread sqlsession池中共有session{}个",
+                                        sqlSessionByThread.size()))
+                                        .append("\n");
+                        for (Entry<Thread, SqlSession> entry : Collections
+                                        .unmodifiableMap(sqlSessionByThread)
+                                        .entrySet())
+                        {
+                                sb.append(StringUtil.getString(
+                                                "线程id[{}]， 线程状态[{}]",
+                                                entry.getKey().toString(),
+                                                entry.getKey().getState()))
+                                                .append("\n");
+                                if (entry.getKey()
+                                                .getState() == Thread.State.TERMINATED)
+                                {
+                                        sb.append("sqlsession泄露\n");
+                                }
+                        }
 
-	void closeSession(SqlSession session, HttpServletRequest request)
-	{
-		try
-		{
-			closeSession(session);
-			sqlSessionByRequest.remove(request);
-		} catch (Exception e)
-		{
-			log.error("", e);
-		}
-	}
+//                        sb.append(StringUtil.getString(
+//                                        "request sqlsession池中共有session{}个",
+//                                        sqlSessionByRequest.size()))
+//                                        .append("\n");
+//                        for (Entry<HttpServletRequest, SqlSession> entry : Collections
+//                                        .unmodifiableMap(sqlSessionByRequest)
+//                                        .entrySet())
+//                        {
+////                                User user = RuntimeUserUtils.INSTANCE
+////                                                .getUserByRequest(
+////                                                                entry.getKey());
+////                                String loginname = user == null ? ""
+////                                                : user.getLoginname();
+//                                sb.append(StringUtil.getString(
+//                                                "request[{}]，user[{}]",
+//                                                entry.getKey().getRequestURI(),
+//                                                "")).append("\n");
+//                        }
 
-	public void doTransact(TransactionHelper helper)
-	{
-		SqlSession session = getSession();
-		try
-		{
-			helper.transact(session);
-		} catch (Exception e)
-		{
-			log.error("执行数据库操作发生异常", e);
-			session.rollback();
-		} finally
-		{
-			closeSession(session);
-		}
-	}
+                        log.debug(sb.toString());
+                } catch (Exception e)
+                {
+                        log.error("打印异常调试信息时出现异常", e);
+                }
+                }
+        }
 
-	public interface TransactionHelper
-	{
-		public void transact(SqlSession session);
+        // public SqlSession getBatchSession()
+        // {
+        // if (sessionFactory == null)
+        // {
+        // throw new
+        // IllegalArgumentException("SqlSessionFactory为空，Mybatis尚未初始化");
+        // }
+        //
+        // return sessionFactory.openSession(ExecutorType.BATCH);
+        // }
 
-	}
+        private void closeSession(SqlSession session)
+        {
+                try
+                {
+                        session.commit();
+                } catch (Exception e)
+                {
+                        log.error("提交到数据库发生错误", e);
+                } finally
+                {
+                        session.close();
+                        sessionCount.getAndDecrement();
+                        log.debug("关闭一个sqlsession，当前数量为：" + sessionCount.get());
+                        debugSessionInfo("");
+                }
+        }
 
-	public void initTestFactory()
-	{
-		try
-		{
-			sessionFactory = new SqlSessionFactoryBuilder()
-			                .build(Resources.getResourceAsReader("configuration_test.xml"));
-		} catch (IOException e)
-		{
-			log.error("", e);
-		}
+        void closeSession(SqlSession session, Thread thread)
+        {
+                closeSession(session);
+                sqlSessionByThread.remove(thread);
+        }
 
-		log.info("mybatis初始化完成");
-	}
+        void closeSession(SqlSession session, HttpServletRequest request)
+        {
+                try
+                {
+                        closeSession(session);
+//                        sqlSessionByRequest.remove(request);
+                } catch (Exception e)
+                {
+                        log.error("", e);
+                }
+        }
 
-	/**
-	 * 直接使用外部初始化的sessionFactory
-	 * 
-	 * @param sessionFactory
-	 */
-	private void initFactory(SqlSessionFactory sessionFactory)
-	{
-		this.sessionFactory = sessionFactory;
-	}
+        public void doTransact(TransactionHelper helper)
+        {
+                SqlSession session = getSession();
+                try
+                {
+                        helper.transact(session);
+                } catch (Exception e)
+                {
+                        log.error("执行数据库操作发生异常", e);
+                        session.rollback();
+                } finally
+                {
+                        closeSession(session);
+                }
+        }
 
-	public static void main(String[] args)
-	{
-		MybatisDao.INSTANCE.initTestFactory();
+        public interface TransactionHelper
+        {
+                public void transact(SqlSession session);
 
-	}
+        }
+
+        public void initTestFactory()
+        {
+                try
+                {
+                        sessionFactory = new SqlSessionFactoryBuilder()
+                                        .build(Resources.getResourceAsReader(
+                                                        "configuration_test.xml"));
+                } catch (IOException e)
+                {
+                        log.error("", e);
+                }
+
+                log.info("mybatis初始化完成");
+        }
+
+        /**
+         * 直接使用外部初始化的sessionFactory
+         * 
+         * @param sessionFactory
+         */
+        private void initFactory(SqlSessionFactory sessionFactory)
+        {
+                this.sessionFactory = sessionFactory;
+        }
+
+        public static void main(String[] args)
+        {
+                MybatisDao.INSTANCE.initTestFactory();
+
+        }
 
 }
