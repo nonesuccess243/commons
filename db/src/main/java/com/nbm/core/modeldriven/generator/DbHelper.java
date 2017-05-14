83f9b500df91eff92a435834218c1cfdf2d1a0a2
niyuzhe <niyuzhe@wayeasoft.com>
2017-05-14 17:32:59 +0800

    在建表时添加关闭connection的逻辑

--- db/src/main/java/com/nbm/core/modeldriven/generator/DbHelper.java
+++ db/src/main/java/com/nbm/core/modeldriven/generator/DbHelper.java
@@ -12,33 +12,63 @@ import com.younker.waf.db.DataSourceProvider;
 
 public class DbHelper
 {
-        private final static Logger log = LoggerFactory.getLogger(DbHelper.class);
-        
-        public static void CREATE_TABLE( String tableName, String sql ) throws SQLException
+        private final static Logger log = LoggerFactory
+                        .getLogger(DbHelper.class);
+
+        public static void CREATE_TABLE(String tableName, String sql)
+                        throws SQLException
         {
-                
-                Connection connection = DataSourceProvider.instance().getDataSource().getConnection();
-                DatabaseMetaData meta = connection.getMetaData();
-                ResultSet resultSet = meta.getTables(null, null, tableName, null);
-                
-                if( resultSet.next())
+                Connection connection = null;
+                try
                 {
-                        log.error("表{}已经存在" + resultSet.getString("TABLE_CAT") + "\t"  
-                                        + resultSet.getString("TABLE_SCHEM") + "\t"  
-                                        + resultSet.getString("TABLE_NAME") + "\t"  
-                                        + resultSet.getString("TABLE_TYPE"), tableName);  
-                }else
+                        connection = DataSourceProvider.instance()
+                                        .getDataSource().getConnection();
+
+                        DatabaseMetaData meta = connection.getMetaData();
+                        ResultSet resultSet = meta.getTables(null, null,
+                                        tableName, null);
+
+                        if (resultSet.next())
+                        {
+                                log.error("表{}已经存在"
+                                                + resultSet.getString(
+                                                                "TABLE_CAT")
+                                                + "\t"
+                                                + resultSet.getString(
+                                                                "TABLE_SCHEM")
+                                                + "\t"
+                                                + resultSet.getString(
+                                                                "TABLE_NAME")
+                                                + "\t"
+                                                + resultSet.getString(
+                                                                "TABLE_TYPE"),
+                                                tableName);
+                        } else
+                        {
+                                log.debug("使用以下sql建表：\n{}", sql);
+                                int result = connection.createStatement()
+                                                .executeUpdate(sql);
+                                if (result == 0)
+                                {
+                                        log.info("创建{}表成功", tableName);
+                                } else
+                                {
+                                        log.error("创建{}表失败", tableName);
+                                }
+                        }
+                } catch (SQLException e)
                 {
-                        log.debug("使用以下sql建表：\n{}", sql);
-                        int result = connection.createStatement().executeUpdate(sql);
-                        if( result == 0 )
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                } finally
+                {
+                        if (connection != null)
                         {
-                                log.info("创建{}表成功", tableName);
-                        }else
-                        {
-                                log.error("创建{}表失败", tableName);
+                                connection.commit();
+                                connection.close();
                         }
                 }
+
         }
 
 }
