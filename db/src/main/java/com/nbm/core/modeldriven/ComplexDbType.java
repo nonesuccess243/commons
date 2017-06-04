2ec37a077ead17cce0246e8b7e0270c5855bf39f
niyuzhe <niyuzhe@wayeasoft.com>
2017-06-04 14:37:25 +0800

    整理注释

--- db/src/main/java/com/nbm/core/modeldriven/ComplexDbType.java
+++ db/src/main/java/com/nbm/core/modeldriven/ComplexDbType.java
@@ -12,6 +12,11 @@ public abstract class ComplexDbType implements Serializable
          */
         private static final long serialVersionUID = 1L;
 
+        
+        /**
+         * 复杂类型在数据库中以一个字符串形式作为查询结果，再采用converter进行转换
+         * @return
+         */
         public abstract Converter getConverter();
 
 }
