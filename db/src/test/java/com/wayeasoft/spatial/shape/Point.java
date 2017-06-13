f12bb7d04bb627004d095aba76920cc85f7cbf96
niyuzhe <niyuzhe@wayeasoft.com>
2017-06-13 15:47:52 +0800

    解决复杂类型的selectByExample和selectByName时的转换异常

---
+++ db/src/test/java/com/wayeasoft/spatial/shape/Point.java
@@ -0,0 +1,83 @@
+package com.wayeasoft.spatial.shape;
+
+
+import org.apache.commons.beanutils.Converter;
+
+import com.nbm.core.modeldriven.DbType;
+import com.nbm.core.modeldriven.anno.DbTemplate;
+import com.nbm.core.modeldriven.anno.DbTypeAnno;
+
+@DbTypeAnno(DbType.POINT)
+@DbTemplate(fetchPrefix="ST_AsText(", 
+        fetchSuffix=")", 
+        populatePrefix = "ST_GeomFromText(", 
+        populateSuffix=")")
+public class Point extends SpatialShape
+{
+        /**
+         * 
+         */
+        private static final long serialVersionUID = 1L;
+        
+        
+        private double x;
+        private double y;
+
+        
+        public Point(){}
+        
+        
+        public double getX()
+        {
+                return x;
+        }
+
+        public void setX(double x)
+        {
+                this.x = x;
+        }
+
+        public double getY()
+        {
+                return y;
+        }
+
+        public void setY(double y)
+        {
+                this.y = y;
+        }
+
+        @Override
+        public String getSqlString()
+        {
+                return "POINT(" + x + " " + y + ")";
+        }
+
+
+        @Override
+        public Converter getConverter()
+        {
+                return new Converter()
+                {
+                        @Override
+                        public Object convert(Class type, Object value)
+                        {
+                                Point point = new Point();
+                                String[] s = ((String)value).replace("POINT", "").replace("(", "").replace(")", "").split(" ");
+                                point.setX(Double.parseDouble(s[0]));
+                                point.setY(Double.parseDouble(s[1]));
+                                return point;
+                        }
+                };
+        }
+
+
+		public Point(double x, double y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+        
+        
+
+}
