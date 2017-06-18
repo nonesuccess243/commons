d706869ba3ddd2b7113aeaac3d9374ea7c0d6b8d
niyuzhe <niyuzhe@wayeasoft.com>
2017-06-18 12:04:27 +0800

    修改缺少运行时注解的错误

--- db/src/main/java/com/nbm/core/modeldriven/anno/Pk.java
+++ db/src/main/java/com/nbm/core/modeldriven/anno/Pk.java
@@ -1,11 +1,14 @@
 package com.nbm.core.modeldriven.anno;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * 名字叫id的字段，自动具备这个属性
  * @author 玉哲
  *
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Pk
 {
 
