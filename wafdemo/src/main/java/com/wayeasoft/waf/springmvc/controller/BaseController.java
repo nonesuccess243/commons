895a09b21eb7a6d7a5333ac053dcf877359ca752
niyuzhe <niyuzhe@wayeasoft.com>
2017-06-23 12:10:31 +0800

    整理代码

--- wafdemo/src/main/java/com/wayeasoft/waf/springmvc/controller/BaseController.java
+++ wafdemo/src/main/java/com/wayeasoft/waf/springmvc/controller/BaseController.java
@@ -3,11 +3,13 @@ package com.wayeasoft.waf.springmvc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import com.wayeasoft.core.modeldriven.dao.CommonDao2;
+import com.wayeasoft.core.modeldriven.dao.CommonDao;
+
+
 
 @Controller
 public class BaseController
 {
         @Autowired
-        protected CommonDao2 dao;
+        protected CommonDao dao;
 }
