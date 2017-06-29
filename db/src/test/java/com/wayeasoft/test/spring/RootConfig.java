eb31b88ac5389e7d3c131b8485e978e03fe423a8
niyuzhe <niyuzhe@wayeasoft.com>
2017-06-29 14:58:16 +0800

    添加注释

--- db/src/test/java/com/wayeasoft/test/spring/RootConfig.java
+++ db/src/test/java/com/wayeasoft/test/spring/RootConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
+
+/**
+ * spring配置类 
+ * @author niyuzhe
+ *
+ */
 @Configuration
 @ImportResource("classpath:spring-config.xml")
 @ComponentScan(basePackages="com.wayeasoft")
