41fceefafc6d976329625f306eb11a9f66308ff9
niyuzhe <niyuzhe@wayeasoft.com>
2017-07-04 10:13:29 +0800

    新建一个dbgrid-web项目，逐步将与dbgrid有关的web文件全部整理到本项目中

--- dbgrid-web/src/main/java/com/wayeasoft/waf/dbgrid/web/DbgridInfoController.java
+++ dbgrid-web/src/main/java/com/wayeasoft/waf/dbgrid/web/DbgridInfoController.java
@@ -1,14 +1,11 @@
-package com.wayeasoft.waf.dbgrid;
+package com.wayeasoft.waf.dbgrid.web;
 
 import com.wayeasoft.waf.springmvc.controller.BaseController;
 import com.younker.waf.dbgrid.DBGrid;
 import com.younker.waf.dbgrid.DBGridEngine;
-import com.younker.waf.dbgrid.DBGrids;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collection;
 
