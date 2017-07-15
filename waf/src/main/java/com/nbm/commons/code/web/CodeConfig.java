f37590f49983f4219d50e3a464edd0bd7846bf68
niyuzhe <niyuzhe@wayeasoft.com>
2017-07-15 17:53:35 +0800

    添加注释

--- waf/src/main/java/com/nbm/commons/code/web/CodeConfig.java
+++ waf/src/main/java/com/nbm/commons/code/web/CodeConfig.java
@@ -12,12 +12,22 @@ package com.nbm.commons.code.web;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * 
+ * 
+ * 
+ * 这是一个servlet listener，用于初始化获取键值对的sql文件。
+ * 
+ * 配置文件固定在/WEB-INF/config/options.properties路径中。
+ * 
+ * 必须以sql.开头，值为一句结果集为两列，第一列的意义为code，第二列的意义为name的sql语句
+ * 
+ * 当初起名字的时候没有以listener为后缀
+ * 
  * @author computer
  * 
  */
