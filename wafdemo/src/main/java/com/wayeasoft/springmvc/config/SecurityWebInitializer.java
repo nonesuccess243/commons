268a38abc30025c34f06447233fb4fd950606334
niyuzhe <niyuzhe@wayeasoft.com>
2018-02-17 21:57:25 +0800

    清理代码

--- wafdemo/src/main/java/com/wayeasoft/springmvc/config/SecurityWebInitializer.java
+++
@@ -1,8 +0,0 @@
-//package com.wayeasoft.springmvc.config;
-//
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//
-//public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer
-//{
-//
-//}
