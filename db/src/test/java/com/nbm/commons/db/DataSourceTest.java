822cc48df2467dd413a3ffe282b1505904a3c67e
niyuzhe <niyuzhe@wayeasoft.com>
2018-02-19 18:04:06 +0800

    完成用配置信息初始化DataSource的过程

--- db/src/test/java/com/nbm/commons/db/DataSourceTest.java
+++ db/src/test/java/com/nbm/commons/db/DataSourceTest.java
@@ -11,32 +11,40 @@ import org.slf4j.LoggerFactory;
 import com.younker.waf.db.DataSourceProvider;
 import com.younker.waf.db.mybatis.MybatisDao;
 
-
 public class DataSourceTest
 {
 
-	private final static Logger log = LoggerFactory
-	                .getLogger(DataSourceTest.class);
+        private final static Logger log = LoggerFactory.getLogger(DataSourceTest.class);
 
-	@BeforeClass
-	public static void setUpBeforeClass()
-	{
+        @BeforeClass
+        public static void setUpBeforeClass()
+        {
 
-	}
+        }
 
-	@Test
-	public void test() throws InstantiationException,
-	                IllegalAccessException, InvocationTargetException, SQLException
-	{
-		DataSourceProvider.initSimple("org.h2.Driver", "jdbc:h2:~/.h2/testdb", "sa", "");
-		
-		
-		DataSourceProvider.instance().runBatch("select 1");
-		
-		MybatisDao.INSTANCE.init();
-		
-//		SqlSessionProvider.openSession();
-//		SqlSessionProvider.getSqlSession().getMapper(ModelDrivenTestModelMapper.class).selectByExample(new ModelDrivenTestModelExample());
-	}
+        @Test
+        public void test()
+                        throws InstantiationException, IllegalAccessException, InvocationTargetException, SQLException
+        {
+                DataSourceProvider.initSimple("org.h2.Driver", "jdbc:h2:~/.h2/testdb", "sa", "");
+
+                DataSourceProvider.instance().runBatch("select 1");
+
+                MybatisDao.INSTANCE.init();
+
+                // SqlSessionProvider.openSession();
+                // SqlSessionProvider.getSqlSession().getMapper(ModelDrivenTestModelMapper.class).selectByExample(new
+                // ModelDrivenTestModelExample());
+        }
+
+        @Test
+        public void testByConfig() throws SQLException
+        {
+                DataSourceProvider.initSimple();
+
+                DataSourceProvider.instance().runBatch("select 1");
+
+                MybatisDao.INSTANCE.initAuto();
+        }
 
 }
