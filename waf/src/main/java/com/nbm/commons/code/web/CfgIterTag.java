ea10ec440cbf131d115259d7736c5a455869f1e2
nick <nick@DESKTOP-FM0TB4J>
2018-03-02 15:57:03 +0800

    按照告诉公司的规则格式化一下

--- waf/src/main/java/com/nbm/commons/code/web/CfgIterTag.java
+++ waf/src/main/java/com/nbm/commons/code/web/CfgIterTag.java
@@ -5,35 +5,44 @@ import java.io.IOException;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
-public class CfgIterTag extends SimpleTagSupport{
+public class CfgIterTag extends SimpleTagSupport
+{
 
-	private String key;
-	private String var;
+        private String key;
+        private String var;
 
-	public String getKey(){
-		return key;
-	}
+        public String getKey()
+        {
+                return key;
+        }
 
-	public void setKey(String key){
-		this.key = key;
-	}
+        public void setKey(String key)
+        {
+                this.key = key;
+        }
 
-	public String getVar(){
-		return var;
-	}
+        public String getVar()
+        {
+                return var;
+        }
 
-	public void setVar(String var){
-		this.var = var;
-	}
+        public void setVar(String var)
+        {
+                this.var = var;
+        }
 
-	@Override
-	public void doTag() throws JspException, IOException{
-		String [] strings = com.wayeasoft.core.configuration.Cfg.I.get(key, String [].class);
-		if (strings != null) {
-			for (String string : strings) {
-				getJspContext().setAttribute(var, string);
-				getJspBody().invoke(null);
-			}
-		}
-	}
+        @Override
+        public void doTag() throws JspException, IOException
+        {
+                String[] strings = com.wayeasoft.core.configuration.Cfg.I
+                                .get(key, String[].class);
+                if (strings != null)
+                {
+                        for (String string : strings)
+                        {
+                                getJspContext().setAttribute(var, string);
+                                getJspBody().invoke(null);
+                        }
+                }
+        }
 }
