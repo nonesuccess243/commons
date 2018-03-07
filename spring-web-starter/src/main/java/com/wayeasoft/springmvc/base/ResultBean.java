78c8043e71ae62ca6e863155d831586481a11df7
niyuzhe <niyuzhe@wayeasoft.com>
2018-03-07 14:52:52 +0800

    add

---
+++ spring-web-starter/src/main/java/com/wayeasoft/springmvc/base/ResultBean.java
@@ -0,0 +1,37 @@
+package com.wayeasoft.springmvc.base;
+
+public class ResultBean<T>
+{
+        private static final long serialVersionUID = 1L;
+
+        public static final int SUCCESS = 0;
+
+        public static final int FAIL = 1;
+
+        public static final int NO_PERMISSION = 2;
+
+        private String msg = "success";
+
+        private int code = SUCCESS;
+
+        private T data;
+
+        public ResultBean()
+        {
+                super();
+        }
+
+        public ResultBean(T data)
+        {
+                super();
+                this.data = data;
+        }
+
+        public ResultBean(Throwable e)
+        {
+                super();
+                this.msg = e.toString();
+                this.code = FAIL;
+        }
+
+}
